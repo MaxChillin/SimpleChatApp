96936afb8f9b6385fba2316b9e4ec84a1559b428
Max Chillin <maxchillin111@gmail.com>
2020-08-16 22:34:07 -0500

    New comments for testing git push

--- app/src/main/java/com/bitlogic/simplechatapp/MainActivity.java
+++ app/src/main/java/com/bitlogic/simplechatapp/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // New comments for testing git push
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
